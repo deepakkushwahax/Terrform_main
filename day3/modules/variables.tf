8c304ee63857438ffa9edc596c626843ba8d2df9
Deepak Kushwaha <88443379+deepakkushwahax@users.noreply.github.com>
2023-10-14 23:48:22 +0530

    Add files via upload

---
+++ day3/modules/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_value" {
+  description = "value of ami"
+}
+
+variable "instance_type" {
+  description = "value of instance type"
+}
+
+variable "subnet_id" {
+  description = "value of subnet id"
+}
